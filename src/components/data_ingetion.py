577f2fa0f226d1d43a365a43938748b937a105a2
Vighnesh <vighneshsankpal7@gmail.com>
2026-01-12 19:08:14 +0530

    data transformation model build.

---
+++ src/components/data_ingetion.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import os
+import sys
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+@dataclass
+class DataIngetionConfig:
+    train_data_path: str= os.path.join("artifacts",'train.csv') 
+    test_data_path: str= os.path.join("artifacts",'test.csv') 
+    raw_data_path: str= os.path.join("artifacts",'raw.csv') 
+
+
+class DataIngetion():
+    def __init__(self):
+        self.ingetion_config = DataIngetionConfig()
+        
+
+    def initiate_data_ingetion(self):
+        try:
+            DATA_PATH='Student_Performance.csv'
+
+            df = pd.read_csv(DATA_PATH)
+
+            #Create the Artifact directory.            
+            os.makedirs(os.path.dirname(self.ingetion_config.train_data_path),exist_ok=True)
+
+            # Save the raw_data.csv file . 
+            df.to_csv(self.ingetion_config.raw_data_path,index=False,header=True)
+
+            train_data, test_data = train_test_split(df, test_size=0.25,random_state=64)
+
+            # Save train.csv file
+            train_data.to_csv(path_or_buf=self.ingetion_config.train_data_path,index=False,header=True)
+
+            # Save test.csv file
+            test_data.to_csv(path_or_buf = self.ingetion_config.test_data_path,index=False,header=True)
+
+
+            return (
+                self.ingetion_config.train_data_path,
+                self.ingetion_config.test_data_path
+            )
+
+        except Exception as e:
+            print("Exception occur in data_ingetion_loading.")
+            print(e)
+
+
+
+from src.components.data_transformation import DataTransformation
+
+if __name__=='__main__':
+    obj = DataIngetion()
+    train_data, test_data = obj.initiate_data_ingetion() 
+
+    obj = DataTransformation()
+    obj.save_preprocessor_object()
---
+++ src/components/data_transformation.py
@@ -0,0 +1,72 @@
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler,OneHotEncoder,FunctionTransformer
+import os
+import joblib
+from dataclasses import dataclass
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path :str = os.path.join("artifacts",'preprocessor.pkl')
+    
+class DataTransformation:
+    def __init__(self):
+        self.transformation_config= DataTransformationConfig()
+    
+    def to_lower_case(self,X):
+        return np.char.lower(X.astype(str))
+    def get_column_transformer_obj(self):
+        try:
+            # We Separet the dataset into two types as,
+            #  1. Numeric columns
+            #  2. categorical column
+
+            # Pipeline for Numeric type features.
+            num_pipeline = Pipeline(steps= [('impute',SimpleImputer(strategy='median')),
+                                            ('scaled',StandardScaler())                                    
+                                    ])
+            
+            # Pipeline for Categorical type features.
+            cat_pipeline = Pipeline(steps=[('impute',SimpleImputer(strategy='most_frequent')),
+                                           ('lowercase',FunctionTransformer(self.to_lower_case,validate=False)),
+                                           ('encoder',OneHotEncoder(handle_unknown='error',sparse_output=False)),
+                                           
+                                    ])
+            # Intigrate this pipeline in column transformer 
+
+            # Numeric features
+            numerical_columns = ['Hours Studied', 'Previous Scores', 'Sleep Hours', 'Sample Question Papers Practiced']
+
+            # Categorical features
+            categorical_columns =['Extracurricular Activities']    
+            
+            # preprocessor object
+
+            preprocessor = ColumnTransformer(transformers=[('num_col',num_pipeline,numerical_columns),
+                                                           ('cat_col',cat_pipeline,categorical_columns)
+                                                           ])
+            
+
+            return preprocessor           
+
+            
+
+        except Exception as e:
+            print("Exception Occur in Preprocessor obj method.")
+            print(e)
+        
+    
+    def save_preprocessor_object(self):
+        '''
+        Save the object in pickle format and return the path
+        '''
+        path = os.path.join('artifacts','preprocessor.pkl')
+
+        joblib.dump(filename=path,value=self.get_column_transformer_obj())
+
+        print("Preprocessor Object saved successfully!")
+        return path
+
+
